ce9be3484b7d023d3c22d9aea166170dfaede69b
rkml14 <119961747+rkml14@users.noreply.github.com>
2023-03-11 11:44:42 -0500

    started reaction-controller

--- controllers/reactionController.js
+++ controllers/reactionController.js
@@ -1,1 +1,16 @@
 const { Thought, Reaction } = require('../models');
+
+module.exports = {
+    createReaction(req, res) {
+        Reaction.find()
+        .then((reaction) => res.json(reaction))
+        .catch((err) => res.status(500).json(err));
+    },
+deleteReaction(req, res) {
+    Reaction.
+}
+
+
+
+
+}
