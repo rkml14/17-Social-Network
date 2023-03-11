09ed53b32c681db957b85c200314dce83d1c3c3e
rkml14 <119961747+rkml14@users.noreply.github.com>
2023-03-11 16:19:40 -0500

    added reaction controller

--- controllers/reactionController.js
+++ controllers/reactionController.js
@@ -1,16 +1,33 @@
 const { Thought, Reaction } = require('../models');
 
 module.exports = {
+    //POST reactiong stored in single thought's reactions array field
     createReaction(req, res) {
-        Reaction.find()
-        .then((reaction) => res.json(reaction))
-        .catch((err) => res.status(500).json(err));
+        Thought.findOneAndUpdate(
+         { _id: req.params.thoughtId },
+         { $push: { reactions: req.body } },
+         { new: true }
+       )
+    
+     .then((Thought) =>
+       !Thought
+         ? res
+             .status(404)
+             .json({ message: 'Reaction created, but no Thoughts with this ID' })
+         : res.json({ message: 'Reaction created' })
+     )
+     .catch((err) => {
+       console.error(err);
+     });
     },
-deleteReaction(req, res) {
-    Reaction.
-}
-
-
-
-
-}
+    //Delete to pull and remove reaction by reactionID value 
+    deleteReaction(req, res) {
+        Reaction.findOneAndDelete({_id: req.params.reactionId })
+        .then((reaction) =>
+        !reaction
+        ? res.status(404).json({ message: 'No Reaction with that ID'})
+        : Reaction.deleteMany({_id: { $in: username.reactions }})
+        )
+        .catch((err) => res.status(500).json(err));
+},
+};
